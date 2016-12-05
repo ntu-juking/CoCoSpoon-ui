38f7b0ff578c2eb646794fb6dcb07d9ed06614f0
bdanglot <bdanglot@gmail.com>
2016-12-05 14:28:50 +0100

    cocospoon ui

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+<img width="10%" src="https://github.com/maxcleme/OPL-Rendu2/blob/master/report/images/logoCoCoSpoon.png" />
+</p>
+# CoCoSpoon
+
+La couverture de code est une métrique représentant le pourcentage de code exécuté. Cette métrique est utiliser lors l'exécution de suites de tests afin de mesurer le code couvert par ces suites.
+
+Certaines méthodes de développement comme le TDD vont garantir une bonne couverture de par le fait que les tests sont écrit avant le code. Une question peut alors se poser, la totalité du code couvert est il bien exécuté en production ? Il est probable qu’une ligne de code exécuté lors d’une suite de tests, ne soit jamais exécutée dans un environnement de production.
+
+Le but est de montrer que calculer la couverture de code sur un programme exécuté dans un environnement de production est possible, de plus, ce calcul pourrait être réalisé en temps réel pour ne pas avoir à stopper l’exécution du code en production afin d’obtenir cette fameuse métrique.
+
+Pour atteindre notre objectif, nous avons utiliser une librairie permettant de faire de la transformation de code source. L’idée est que le programme transformé puisse d’auto-instrumenter afin de notifier l’utilisateur de sa couverture à n'importe quel moment de son exécution.
+
+# Usage
+
+    CocoSpoon
+       -i, --input-path     input project folder
+       -o, --output-path    instrumented project destination
+      [-c, --classpath      classpath]
+      [-v, --view-type      TEXT | OVERALL | INTERACTIVE]
+
+# Screencast
+
+[![alt tag](https://github.com/maxcleme/OPL-Rendu2/blob/master/report/images/youtubeVideo.png)](https://youtu.be/QUZK3erdiEM)
+
+
+# Screenshots
+
+![alt tag](https://github.com/maxcleme/OPL-Rendu2/blob/master/report/images/overall_view.png)
+![alt tag](https://github.com/maxcleme/OPL-Rendu2/blob/master/report/images/package_view.png)
+![alt tag](https://github.com/maxcleme/OPL-Rendu2/blob/master/report/images/couverture_classe.png)
---
+++ src/main/java/fil/iagl/opl/cocospoon/tools/Params.java
@@ -0,0 +1,95 @@
+package fil.iagl.opl.cocospoon.tools;
+
+import java.util.Locale;
+
+import com.sanityinc.jargs.CmdLineParser;
+import com.sanityinc.jargs.CmdLineParser.IllegalOptionValueException;
+import com.sanityinc.jargs.CmdLineParser.Option;
+
+import instrumenting._View.View;
+
+public class Params {
+
+  private String inputSource;
+  private String outputSource;
+  private String classpath;
+  private View view;
+
+  public void handleArgs(String[] args) {
+    CmdLineParser parse = new CmdLineParser();
+
+    Option<String> inputSourceOption = parse.addStringOption('i', "input-path");
+    Option<String> outputSourceOption = parse.addStringOption('o', "output-path");
+    Option<String> classPathOption = parse.addStringOption('c', "classpath");
+    Option<View> viewOption = parse.addOption(new ViewOption('v', "view-type", true));
+
+    try {
+      parse.parse(args);
+    } catch (CmdLineParser.OptionException e) {
+      System.err.println(e.getMessage());
+      printUsage();
+      System.exit(2);
+    }
+
+    inputSource = parse.getOptionValue(inputSourceOption);
+    outputSource = parse.getOptionValue(outputSourceOption);
+    classpath = parse.getOptionValue(classPathOption);
+    view = parse.getOptionValue(viewOption);
+
+    if (view == null) {
+      view = View.OVERALL;
+    }
+
+    if (inputSource == null || outputSource == null) {
+      printUsage();
+      System.exit(2);
+    }
+  }
+
+  private void printUsage() {
+    System.err.println("Usage: CoCoSpoon\n"
+      + " -i, --input-path\t\tpath_program\n"
+      + " -o, --output-path\t\tinstrumented sources destination\n"
+      + "[-c, --classpath]\t\tclasspath\n"
+      + "[-v, --view-type]\t\tview type : | OVERALL | INTERACTIVE | TEXT \n");
+  }
+
+  public String getClasspath() {
+    return classpath;
+  }
+
+  public void setClasspath(String classpath) {
+    this.classpath = classpath;
+  }
+
+  public String getInputSource() {
+    return inputSource;
+  }
+
+  public String getOutputSource() {
+    return outputSource;
+  }
+
+  public View getView() {
+    return view;
+  }
+
+  private class ViewOption extends Option<View> {
+
+    protected ViewOption(char shortForm, String longForm, boolean wantsValue) {
+      super(shortForm, longForm, wantsValue);
+    }
+
+    @Override
+    protected View getDefaultValue() {
+      return View.INTERACTIVE;
+    }
+
+    @Override
+    protected View parseValue(String arg, Locale locale) throws IllegalOptionValueException {
+      return View.valueOf(arg);
+    }
+
+  }
+
+}
